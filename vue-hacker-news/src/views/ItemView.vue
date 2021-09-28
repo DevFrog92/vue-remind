0febb7c6ff9935cd9391ebfd1a52171e1d0c94a1
DevFrog92 <yongsu1481@gmail.com>
2021-09-28 17:43:53 +0900

    add : ask item view

--- vue-hacker-news/src/views/ItemView.vue
+++ vue-hacker-news/src/views/ItemView.vue
@@ -1,13 +1,31 @@
 <template>
   <div>
-    <h1>
-      {{ item.title }}
-    </h1>
-    <p class="item_content" />
+    <section>
+      <!-- 질문 상세 정보 -->
+      <div class="user_container">
+        <div>
+          <i class="fas fa-user" />
+        </div>
+        <div class="user_desc">
+          <router-link :to="`/user/${item.user}`">
+            {{ item.user }}
+          </router-link>
+          <div class="time">
+            {{ item.time_ago }}
+          </div>
+        </div>
+      </div>
+      <h2>{{ item.title }}</h2>
+      <div class="content" />
+    </section>
+    <section>
+      <!-- 댓글  -->
+    </section>
   </div>
 </template>
 
 <script>
+import { mapGetters, } from "vuex"
 export default {
   data() {
     return {
@@ -15,9 +33,9 @@ export default {
     }
   },
   computed: {
-    item() {
-      return this.$store.state.askItem
-    },
+    ...mapGetters({
+      item: "fetchedItem",
+    }),
   },
   watch: {
     item() {
@@ -25,7 +43,7 @@ export default {
     },
   },
   mounted() {
-    this.itemContentElem = document.querySelector(".item_content")
+    this.itemContentElem = document.querySelector(".content")
     this.setContent()
   },
   created() {
@@ -35,13 +53,28 @@ export default {
   },
   methods: {
     setContent() {
-      console.log(this.item)
       this.itemContentElem.innerHTML = this.item.content
     },
   },
 }
 </script>
 
-<style>
+<style scoped>
+.user_container {
+  padding:0.5rem;
+  display: flex;
+  align-items: center;
+}
 
+.fa-user {
+  font-size: 2.5rem;
+}
+
+.user_desc {
+  padding-left: 8px;
+}
+
+.time {
+  font-size: 0.7rem;
+}
 </style>
