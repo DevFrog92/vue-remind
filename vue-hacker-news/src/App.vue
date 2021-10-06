548b6fea27a1d868e1fb86dc942162eba49244fd
DevFrog92 <yongsu1481@gmail.com>
2021-10-06 23:36:54 +0900

    add : event bus

--- vue-hacker-news/src/App.vue
+++ vue-hacker-news/src/App.vue
@@ -4,22 +4,41 @@
     <transition name="page">
       <router-view />
     </transition>
-    <Spinner :loading="true" />
+    <Spinner :loading="loadingStatus" />
   </div>
 </template>
 
 <script>
 import ToolBar from "@/components/ToolBar"
 import Spinner from "@/components/Spinner"
+import bus from "./utils/bus.js"
 
 export default {
   components: {
     ToolBar,
     Spinner,
   },
+  data() {
+    return {
+      loadingStatus: false,
+    }
+  },
   created() {
+    bus.$on("start:spinner", this.startSpinner)
+    bus.$on("end:spinner", this.endSpinner)
+  },
+  beforeDestroy() {
+    // event bus에 event가 계속 쌓이기 때문에 해제를 해주어야 한다.
+    bus.$off("start:spinner", this.startSpinner)
+    bus.$off("end:spinner", this.endSpinner)
   },
   methods: {
+    startSpinner() {
+      this.loadingStatus = true
+    },
+    endSpinner() {
+      this.loadingStatus = false
+    },
   },
 }
 </script>
