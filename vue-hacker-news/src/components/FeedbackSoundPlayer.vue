936738c135d10d1b2b369d1b7a5646f97b3eef00
DevFrog92 <yongsu1481@gmail.com>
2021-12-02 11:30:27 +0900

    add: sound feedback component

---
+++ vue-hacker-news/src/components/FeedbackSoundPlayer.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="sound_player_wrapper">
+    <audio
+      v-if="audioFileName === 'rich'"
+      class="audio_elem"
+      type="audio/mp3"
+    />
+    <audio
+      v-if="audioFileName === 'rich'"
+      class="audio_elem_coin"
+      type="audio/mp3"
+    />
+    <audio
+      v-if="audioFileName === 'lean'"
+      class="audio_elem"
+      type="audio/mp3"
+    />
+    <audio
+      v-if="audioFileName === 'lean'"
+      class="audio_elem_coin"
+      type="audio/mp3"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    audioFileName: {
+      type: String,
+      default: "",
+    },
+  },
+  data() {
+    return {
+      audioElem: null,
+      audioElem2: null,
+    }
+  },
+  mounted() {
+    this.getAudioElem()
+  },
+  methods: {
+    getAudioElem() {
+      this.audioElem = document.querySelector(".audio_elem")
+      this.audioElem2 = document.querySelector(".audio_elem_coin")
+      this.playFeedbackSound(this.audioElem, this.audioElem2)
+    },
+    playFeedbackSound(audioElem, audioElem2) {
+      audioElem.volume = 0.53
+      audioElem2.volume = 0.55
+      audioElem.play()
+      audioElem2.play()
+    },
+  },
+}
+</script>
